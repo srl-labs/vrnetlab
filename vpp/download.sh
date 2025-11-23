a69ce38f7de96666a97263162c5411a00c90cdfc
WeiLuo <35321202+BurlyLuo@users.noreply.github.com>
2025-11-23 11:49:00 +0800

    Update download script for VPP file naming

--- vpp/download.sh
+++ vpp/download.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-file=vpp-proto-bookworm-20250607.qcow2
+filename=vpp-proto-bookworm.qcow2
 # Check if the file already exists in the current directory
 if [ -e "$filename" ]; then
     echo "File $filename already exists. Skipping download."
 else
-    wget https://ipng.ch/media/vpp-proto/vpp-proto-bookworm-20250607.qcow2.lrz
+    wget https://ipng.ch/media/vpp-proto/vpp-proto-bookworm.qcow2.lrz
     apt install lrzip
-    lrzip -d vpp-proto-bookworm-20250607.qcow2.lrz
+    lrzip -d vpp-proto-bookworm.qcow2.lrz
 fi
