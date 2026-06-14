5ab84e179694c61b3c58d0dd4bea3a71f6b7f45c
Sam Burney <sburney@sifnt.net.au>
2026-06-14 17:38:35 +0930

    bugfix: Added `speed` and `duplex` parameters to `nic_type`.

--- mikrotik/routeros/docker/launch.py
+++ mikrotik/routeros/docker/launch.py
@@ -79,7 +79,7 @@ class ROS_vm(vrnetlab.VM):
 
         self.hostname = hostname
         self.conn_mode = conn_mode
-        self.nic_type = "virtio-net"  # "e1000" is default but breaks mtu > 1500 on vlan subinterfaces on RouterOS 6.x.x
+        self.nic_type = "virtio-net,speed=10000,duplex=full"  # "e1000" is default but breaks mtu > 1500 on vlan subinterfaces on RouterOS 6.x.x
         self.num_nics = 31
 
         # set up bridge for management interface to a localhost
